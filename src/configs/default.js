ea9f17fd961502044417e03d426eb9f46de125ca
Sherbakov Mikhail <michailshcherbakov@gmail.com>
2024-03-29 10:07:27 +0300

    fix(ts): ignoring unnecessary folders

--- src/configs/default.js
+++ src/configs/default.js
@@ -9,6 +9,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    ignores: ["node_modules", "dist", "coverage"],
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: "module",
